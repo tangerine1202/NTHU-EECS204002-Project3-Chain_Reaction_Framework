c7988a0fdf3d57c9a50a32dea4ebb434053d3e0b
Alan Huang <alan0988469026@gmail.com>
2020-12-31 01:54:23 +0800

    feat: add basic manual algo

--- source/manual.cpp
+++ source/manual.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 /******************************************************
- * In your algorithm, you can just use the the funcitons
- * listed by TA to get the board information.(functions 
+ * In your algorithm, you can just use the the functions
+ * listed by TA to get the board information.(functions
  * 1. ~ 4. are listed in next block)
- * 
+ *
  * The STL library functions is not allowed to use.
 ******************************************************/
 
@@ -18,17 +18,24 @@ using namespace std;
  * 2. int board.get_capacity(int row_index, int col_index)
  * 3. char board.get_cell_color(int row_index, int col_index)
  * 4. void board.print_current_board(int row_index, int col_index, int round)
- * 
+ *
  * 1. The function that return the number of orbs in cell(row, col)
  * 2. The function that return the orb capacity of the cell(row, col)
  * 3. The function that return the color fo the cell(row, col)
  * 4. The function that print out the current board statement
 *************************************************************************/
 
+void algorithm_A(Board board, Player player, int index[])
+{
+    static int round = 0;
+    static int row, col;
 
-void algorithm_A(Board board, Player player, int index[]){
+    cout << "Player color: " << player.get_color() << '\n';
+    board.print_current_board(row, col, round++);
 
-    //////your algorithm design///////////
-   
-    
+    cin >> row >> col;
+
+    // place orb
+    index[0] = row;
+    index[1] = col;
 }
