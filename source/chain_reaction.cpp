c61ec158b1a44fc31919c5173666f0abe7fd546a
Alan Huang <alan0988469026@gmail.com>
2021-01-02 22:14:09 +0800

    chroe: code style

--- source/chain_reaction.cpp
+++ source/chain_reaction.cpp
@@ -6,7 +6,8 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
     Board board;
     Player red_player(RED);
@@ -16,18 +17,21 @@ int main(){
     int round = 1;
     int index[2];
 
-    while(1){
+    while (1)
+    {
 
         //////////// Red Player operations ////////////
         algorithm_A(board, red_player, index);
         board.place_orb(index[0], index[1], &red_player);
 
-        if(rules_violation(red_player)) return 0;
+        if (rules_violation(red_player))
+            return 0;
 
         board.print_current_board(index[0], index[1], round);
         round++;
 
-        if(board.win_the_game(red_player) && !first_two_step){
+        if (board.win_the_game(red_player) && !first_two_step)
+        {
             cout << "Red Player won the game !!!" << endl;
             return 0;
         }
@@ -36,12 +40,14 @@ int main(){
         algorithm_B(board, blue_player, index);
         board.place_orb(index[0], index[1], &blue_player);
 
-        if(rules_violation(blue_player)) return 0;
-        
+        if (rules_violation(blue_player))
+            return 0;
+
         board.print_current_board(index[0], index[1], round);
         round++;
 
-        if(board.win_the_game(blue_player) && !first_two_step){
+        if (board.win_the_game(blue_player) && !first_two_step)
+        {
             cout << "Blue Player won the game !!!" << endl;
             return 0;
         }
@@ -50,4 +56,4 @@ int main(){
     }
 
     return 0;
-} 
+}
